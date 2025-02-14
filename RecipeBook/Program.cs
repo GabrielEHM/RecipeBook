0fe600c4833e2e7d4bf3973321872d9288035c4c
Gabriel Hernández <ghernandez@teravisiontech.com>
2025-02-14 08:28:33 -0400

    Add project files.

---
+++ RecipeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+
+var connection = new SqlConnection(config.GetConnectionString("RecipeBook"));
+
+var ingredients = connection.Query("SELECT * FROM Ingredients").ToList();
+
+foreach (var ingredient in ingredients)
+{
+    Console.WriteLine(JsonConvert.SerializeObject(ingredient));
+}
